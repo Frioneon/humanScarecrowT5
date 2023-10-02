234b7ccf185a927e59ec90b14807fff50099e8ec
Frioneon <zacarnoldmb@gmail.com>
2023-10-02 16:10:34 -0400

    Collison currento

--- humanScarecrow_Unity/Assets/PlayerMove.cs
+++ humanScarecrow_Unity/Assets/PlayerMove.cs
@@ -17,15 +17,23 @@
         void Update() {
             if (Input.GetKey("up")||Input.GetKey("w")) {
                 spriteRenderer.sprite = spriteList[3];
+                GetComponent<BoxCollider2D>().size = new Vector2(1f, 3f);
+                GetComponent<BoxCollider2D>().offset = new Vector2(0f, 1f);
             }
             if (Input.GetKey("down")||Input.GetKey("s")) {
                 spriteRenderer.sprite = spriteList[0];
+                GetComponent<BoxCollider2D>().size = new Vector2(1f, 3f);
+                GetComponent<BoxCollider2D>().offset = new Vector2(0f, -1f);
             }
             if (Input.GetKey("right")||Input.GetKey("d")) {
                 spriteRenderer.sprite = spriteList[2];
+                GetComponent<BoxCollider2D>().size = new Vector2(3f, 1f);
+                GetComponent<BoxCollider2D>().offset = new Vector2(1f, 0f);
             }
             if (Input.GetKey("left")||Input.GetKey("a")) {
                 spriteRenderer.sprite = spriteList[1];
+                GetComponent<BoxCollider2D>().size = new Vector2(3f, 1f);
+                GetComponent<BoxCollider2D>().offset = new Vector2(-1f, 0f);
             }
         }
 
